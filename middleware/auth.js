8988b6f08726994b1e68363d87984d7ce8e5ae6e
DevJulBrd <mjulien.bernard@icloud.com>
2023-02-22 16:25:46 +0100

    OK

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,10 @@
 const jwt = require('jsonwebtoken');
 
+
 module.exports = (req, res, next) => {
     try{
-        const token = req.headers.authorization.split('')[1];
-        const decodedToken = jwt.verify(token, 'RANDOM-SECRET-TOKEN');
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, `${process.env.TOKEN}`);
         const userId = decodedToken.userId;
         req.auth = {
             userId: userId
