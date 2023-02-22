51d30df851e28d08bee38618dd315716bda40bec
DevJulBrd <mjulien.bernard@icloud.com>
2023-02-22 16:26:23 +0100

    Ok

--- controllers/user.js
+++ controllers/user.js
@@ -16,7 +16,31 @@ exports.signup = (req, res, next) => {
         .catch((error) => res.status(400).json({error}));
     })
     .catch((error) => res.status(500).json({error}));
-    
+};
 
-
+exports.login = (req, res, next) => {
+    User.findOne({email: req.body.email})
+    .then((user) => {
+        if(!user){
+            return res.status(401).json({error: "Email/Mot de passe incorrect"})
+        } else {
+            bcrypt.compare(req.body.password, user.password)
+            .then((valid) => {
+                if(!valid){
+                    return res.status(401).json({error: "Email/Mot de passe incorrect"})
+                } else {
+                    res.status(200).json({
+                        userId: user._id,
+                        token: jwt.sign(
+                            {userId: user._id},
+                            `${process.env.TOKEN}`,
+                            {expiresIn: '12h'}
+                        )
+                    })
+                }
+            })
+            .catch((error) => res.status(500).json({error}));
+        }
+    })
+    .catch((error) => res.status(500).json({error}));
 };
